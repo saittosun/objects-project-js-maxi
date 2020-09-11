3844745c2dd3c6178a7d10aabbac0959dd65c41b
tostos <saittosun2@gmail.com>
2020-09-11 04:43:31 +0200

    for in loops & outputting dynamic properties

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -17,7 +17,16 @@ const renderMovies = () => {
 
   movies.forEach((movie) => {
     const movieEl = document.createElement("li");
-    movieEl.textContent = movie.info.title;
+    // movieEl.textContent = movie.info.title;
+    let text = movie.info.title + "-";
+    // With that we can go through all keys in an object
+    for (const key in movie.info) {
+      // this has to be a string because otherwise if you write it like this, Javascript would look for a variable named title which it won't find in this function and it would then use the value stored in this variable for this comparison. So instead since keys are strings as you learned, we compared it to a string here and if the key is not title, then we know we're looking at the property the user entered. Well and this is then what I want to add to my text.
+      if (key !== "title") {
+        text = text + `${key}: ${movie.info[key]}`;
+        movieEl.textContent = text;
+      }
+    }
     movieList.append(movieEl);
   });
 };
